3123b412d57c10a4caaded3881adbc46ff0a1199
Julien Lenne <contact.lenne@gmail.com>
2025-12-03 09:52:56 +0800

    Fix deprecated zsh initExtra -> initContent

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -54,7 +54,7 @@
     };
     
     # Additional shell configuration
-    initExtra = ''
+    initContent = ''
       # Powerlevel10k instant prompt
       if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
