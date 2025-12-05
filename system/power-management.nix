d37904ec47c9bac9f10a7ca78104295728314634
Julien Lenne <contact.lenne@gmail.com>
2025-12-06 05:03:12 +0800

    Fix power management: use native Nix script instead of external file
    - Embedded script in power-management.nix using writeShellScriptBin
    - Uses full paths to binaries (works in udev context)
    - AC plugged: balanced + 80% brightness
    - Battery: power-saver + 40% brightness
    - Removed external script file

--- system/power-management.nix
+++ system/power-management.nix
@@ -1,24 +1,37 @@
 { config, pkgs, ... }:
 
+let
+  powerProfileScript = pkgs.writeShellScriptBin "power-profile-switch" ''
+    # Get AC adapter status
+    AC_STATUS=$(cat /sys/class/power_supply/AC/online)
+
+    if [ "$AC_STATUS" -eq 1 ]; then
+        # AC plugged in: balanced mode, 80% brightness
+        ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
+        ${pkgs.brightnessctl}/bin/brightnessctl set 80%
+        ${pkgs.util-linux}/bin/logger "Power: AC connected - switched to balanced profile with 80% brightness"
+    else
+        # AC unplugged: power-saver mode, 40% brightness
+        ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
+        ${pkgs.brightnessctl}/bin/brightnessctl set 40%
+        ${pkgs.util-linux}/bin/logger "Power: AC disconnected - switched to power-saver profile with 40% brightness"
+    fi
+  '';
+in
 {
   # Copy power profile switch script
   environment.systemPackages = with pkgs; [
     brightnessctl
+    powerProfileScript
   ];
 
   # Udev rule to detect AC adapter changes
   services.udev.extraRules = ''
     # Detect AC adapter changes and switch power profile + brightness
-    SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${pkgs.bash}/bin/bash /etc/nixos/power-profile-switch.sh"
-    SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${pkgs.bash}/bin/bash /etc/nixos/power-profile-switch.sh"
+    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="0", RUN+="${powerProfileScript}/bin/power-profile-switch"
+    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="1", RUN+="${powerProfileScript}/bin/power-profile-switch"
   '';
 
   # Enable power-profiles-daemon
   services.power-profiles-daemon.enable = true;
-
-  # Copy the power profile switch script to /etc/nixos
-  environment.etc."nixos/power-profile-switch.sh" = {
-    source = ./scripts/power-profile-switch.sh;
-    mode = "0755";
-  };
 }
