9381a25e99f427f6b17b40db918b4d25dfb18ee4
Julien Lenne <contact.lenne@gmail.com>
2025-12-06 05:24:28 +0800

    Remove useless notifications - DankShell bar shows the change

--- system/power-management.nix
+++ system/power-management.nix
@@ -4,7 +4,6 @@
   # Install brightnessctl
   environment.systemPackages = with pkgs; [
     brightnessctl
-    libnotify  # for notify-send
   ];
 
   # Enable power-profiles-daemon
@@ -22,24 +21,12 @@
           if [ "$AC_STATUS" -eq 1 ]; then
               # AC plugged in: balanced mode, 80% brightness
               ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
-              
-              # Set brightness via DMS to update the UI bar
               ${pkgs.su}/bin/su julien -c "${pkgs.coreutils}/bin/env DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.coreutils}/bin/env HOME=/home/julien /run/current-system/sw/bin/dms ipc call brightness set 80 'backlight:amdgpu_bl1'"
-              
-              # Send notification to user
-              ${pkgs.su}/bin/su julien -c "DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.libnotify}/bin/notify-send -u low -t 2000 '⚡ AC Connected' 'Profile: Balanced • Brightness: 80%'"
-              
               ${pkgs.util-linux}/bin/logger "Power: AC connected - switched to balanced profile with 80% brightness"
           else
               # AC unplugged: power-saver mode, 40% brightness
               ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
-              
-              # Set brightness via DMS to update the UI bar
               ${pkgs.su}/bin/su julien -c "${pkgs.coreutils}/bin/env DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.coreutils}/bin/env HOME=/home/julien /run/current-system/sw/bin/dms ipc call brightness set 40 'backlight:amdgpu_bl1'"
-              
-              # Send notification to user
-              ${pkgs.su}/bin/su julien -c "DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.libnotify}/bin/notify-send -u low -t 2000 '🔋 Battery Mode' 'Profile: Power Saver • Brightness: 40%'"
-              
               ${pkgs.util-linux}/bin/logger "Power: AC disconnected - switched to power-saver profile with 40% brightness"
           fi
         '';
