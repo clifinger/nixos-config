1586d57f07fd96f345dc6e746d07591373ec320a
Julien Lenne <contact.lenne@gmail.com>
2025-12-06 05:12:04 +0800

    Add desktop notifications for power profile changes
    - Show notification when AC plugged/unplugged
    - Battery: 🔋 Power Saver • 40%
    - AC: ⚡ Balanced • 80%
    - Helps user see the change in DankShell

--- system/power-management.nix
+++ system/power-management.nix
@@ -4,6 +4,7 @@
   # Install brightnessctl
   environment.systemPackages = with pkgs; [
     brightnessctl
+    libnotify  # for notify-send
   ];
 
   # Enable power-profiles-daemon
@@ -22,11 +23,21 @@
               # AC plugged in: balanced mode, 80% brightness
               ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
               ${pkgs.brightnessctl}/bin/brightnessctl set 80%
+              
+              # Send notification to user
+              DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
+                ${pkgs.libnotify}/bin/notify-send -u low -t 2000 "⚡ AC Connected" "Profile: Balanced • Brightness: 80%"
+              
               ${pkgs.util-linux}/bin/logger "Power: AC connected - switched to balanced profile with 80% brightness"
           else
               # AC unplugged: power-saver mode, 40% brightness
               ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
               ${pkgs.brightnessctl}/bin/brightnessctl set 40%
+              
+              # Send notification to user
+              DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
+                ${pkgs.libnotify}/bin/notify-send -u low -t 2000 "🔋 Battery Mode" "Profile: Power Saver • Brightness: 40%"
+              
               ${pkgs.util-linux}/bin/logger "Power: AC disconnected - switched to power-saver profile with 40% brightness"
           fi
         '';
