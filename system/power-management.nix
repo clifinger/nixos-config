2f27814584fd32beaa7aed3703292a61ce28c53f
Julien Lenne <contact.lenne@gmail.com>
2025-12-06 05:15:27 +0800

    Run brightnessctl as user to update DankShell UI
    - acpid runs as root but DankShell needs user context
    - Use 'su julien' to run brightnessctl as user
    - This ensures DankShell brightness bar updates properly
    - Added udev rule to allow video group brightness changes

--- system/power-management.nix
+++ system/power-management.nix
@@ -10,6 +10,11 @@
   # Enable power-profiles-daemon
   services.power-profiles-daemon.enable = true;
 
+  # Allow users in video group to change brightness without sudo
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video $sys$devpath/brightness", RUN+="${pkgs.coreutils}/bin/chmod g+w $sys$devpath/brightness"
+  '';
+
   # Enable acpid to handle power events
   services.acpid = {
     enable = true;
@@ -22,21 +27,23 @@
           if [ "$AC_STATUS" -eq 1 ]; then
               # AC plugged in: balanced mode, 80% brightness
               ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
-              ${pkgs.brightnessctl}/bin/brightnessctl set 80%
+              
+              # Run as user julien to update DankShell UI
+              ${pkgs.su}/bin/su julien -c "${pkgs.brightnessctl}/bin/brightnessctl set 80%"
               
               # Send notification to user
-              DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
-                ${pkgs.libnotify}/bin/notify-send -u low -t 2000 "⚡ AC Connected" "Profile: Balanced • Brightness: 80%"
+              ${pkgs.su}/bin/su julien -c "DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.libnotify}/bin/notify-send -u low -t 2000 '⚡ AC Connected' 'Profile: Balanced • Brightness: 80%'"
               
               ${pkgs.util-linux}/bin/logger "Power: AC connected - switched to balanced profile with 80% brightness"
           else
               # AC unplugged: power-saver mode, 40% brightness
               ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
-              ${pkgs.brightnessctl}/bin/brightnessctl set 40%
+              
+              # Run as user julien to update DankShell UI
+              ${pkgs.su}/bin/su julien -c "${pkgs.brightnessctl}/bin/brightnessctl set 40%"
               
               # Send notification to user
-              DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
-                ${pkgs.libnotify}/bin/notify-send -u low -t 2000 "🔋 Battery Mode" "Profile: Power Saver • Brightness: 40%"
+              ${pkgs.su}/bin/su julien -c "DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.libnotify}/bin/notify-send -u low -t 2000 '🔋 Battery Mode' 'Profile: Power Saver • Brightness: 40%'"
               
               ${pkgs.util-linux}/bin/logger "Power: AC disconnected - switched to power-saver profile with 40% brightness"
           fi
