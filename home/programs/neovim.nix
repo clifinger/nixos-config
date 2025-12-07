96388388bc2b2a52970ba7101aba05e2f42ca9ec
Julien Lenne <contact.lenne@gmail.com>
2025-12-07 11:57:27 +0800

    Remove nvim home-manager config, keep only the app

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -1,53 +1,40 @@
 { config, pkgs, ... }:
 
 {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    
-    extraPackages = with pkgs; [
-      # Build essentials
-      gcc
-      gnumake
-      unzip
-      readline
-      
-      # Lua and LuaRocks for magick
-      lua51Packages.luarocks-nix
-      
-      # Search tools
-      ripgrep
-      fd
-      
-      # Git UI
-      lazygit
-      
-      # Tree-sitter
-      tree-sitter
-      
-      # Runtimes needed by Mason
-      nodejs
-      python3
-      go
-      cargo
-      rustc
-      
-      # Image preview for Telescope
-      ueberzugpp
-      imagemagick
-      
-      # Version manager for additional tools
-      mise
-    ];
-  };
+  programs.neovim.enable = true;
   
-  # Clone your nvim config
-  home.activation.cloneNvimConfig = config.lib.dag.entryAfter ["writeBoundary"] ''
-    if [ ! -d "${config.home.homeDirectory}/.config/nvim" ]; then
-      ${pkgs.git}/bin/git clone git@github.com:clifinger/nvim-for-dev.git ${config.home.homeDirectory}/.config/nvim
-    fi
-  '';
+  home.packages = with pkgs; [
+    # Build essentials
+    gcc
+    gnumake
+    unzip
+    readline
+    
+    # Lua and LuaRocks for magick
+    lua51Packages.luarocks-nix
+    
+    # Search tools
+    ripgrep
+    fd
+    
+    # Git UI
+    lazygit
+    
+    # Tree-sitter
+    tree-sitter
+    
+    # Runtimes needed by Mason
+    nodejs
+    python3
+    go
+    cargo
+    rustc
+    
+    # Image preview for Telescope
+    ueberzugpp
+    imagemagick
+    
+    # Version manager for additional tools
+    mise
+  ];
 }
--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    ./hardware-configuration.nix
+    /etc/nixos/hardware-configuration.nix
     ../../system/boot.nix
     ../../system/networking.nix
     ../../system/audio.nix
